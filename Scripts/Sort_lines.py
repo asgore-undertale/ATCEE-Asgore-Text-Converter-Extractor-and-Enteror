41e506e84df29fe2cb16169920ae6bd20a4ad947
asgore-undertale <52295461+asgore-undertale@users.noreply.github.com>
2021-01-16 19:29:53 +0200

    Delete Scripts directory

--- Scripts/Sort_lines.py
+++
@@ -1,8 +0,0 @@
-def script(text, case = 'short to long'):
-    lines_list = text.split('\n')
-    lines_list.sort(key=len)
-    
-    if case == 'long to short': lines_list = lines_list[::-1]
-    text = '\n'.join(lines_list)
-    
-    return text
